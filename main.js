f5369b3b6014eb3205a81b1fb35d9564024376df
JoJoJux (aka. J0J0HA) <johannes@jojojux.de>
2026-04-14 20:19:15 +0200

    Reformat & fix import mapposition

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/GLTFLoader.js";
 
 // Canvas & Container
 const canvas = document.getElementById("canvas");
